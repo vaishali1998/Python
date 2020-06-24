d139a8b20e809e58950e454fbcd40746d55e5bff
Vaishali Kumari <30860160+vaishali1998@users.noreply.github.com>
2020-06-24 15:10:18 +0530

    Update wordc.py

--- wordc/wordc.py
+++ wordc/wordc.py
@@ -1,3 +1,16 @@
+print("                                                     o        o__ __o   ")
+print("                                                    <|>      /v     v\  ")
+print("                                                    < \     />       <\ ")
+print("  o              o     o__ __o    \o__ __o     o__ __o/   o/            ")
+print(" <|>            <|>   /v     v\    |     |>   /v     |   <|             ")
+print(" < >            < >  />       <\  / \   < >  />     / \   \\            ")
+print("  \o    o/\o    o/   \         /  \o/        \      \o/     \         / ")
+print("   v\  /v  v\  /v     o       o    |          o      |       o       o  ")
+print("    <\/>    <\/>      <\__ __/>   / \         <\__  / \      <\__ __/>  ")
+print("\n")
+print("A Simple Python Code to count number of words in any web page") 
+print("\n")
+
 import requests
 
 url = raw_input("Enter URL: ")
@@ -13,7 +26,8 @@ i=1
 while(i):
 	word = raw_input("Enter word to be count: ")
 	count = data.count(word)
-	print("Occurrence of the word:" +str(count))
+	print("Occurrence of the word "+word+": "+str(count))
 
 	if word == 'exit':
 		i=0
+
